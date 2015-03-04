a7620624fc4d3bba9dc5334cbe69b26fd8cd7db6
b98606021 <b98606021@ntu.edu.tw>
2015-03-04 21:43:30 +0800

    Finish video1.jade

--- routes/video.js
+++ routes/video.js
@@ -1,7 +1,7 @@
 exports.video1 = function(req, res, next) {
-	res.render('video', {title:'video1'});
+	res.render('video1', {title:'video1'});
 };
 
 exports.video2 = function(req, res, next) {
-	res.render('video', {title:'video2'});
+	res.render('video2', {title:'video2'});
 };
